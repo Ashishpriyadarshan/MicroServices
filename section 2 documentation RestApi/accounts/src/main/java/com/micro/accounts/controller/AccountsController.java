19ad7afabd906d43a81f38bbbafb3e54109d8c94
Ashish <ashishpriyadarshan2605@gmail.com>
2026-02-17 04:53:02 +0530

    Controller information using Tag annotation | Operation , ApiResponse and ApiResponses Annoation on REST API methods

--- section 2 documentation RestApi/accounts/src/main/java/com/micro/accounts/controller/AccountsController.java
+++ section 2 documentation RestApi/accounts/src/main/java/com/micro/accounts/controller/AccountsController.java
@@ -6,6 +6,10 @@ import com.micro.accounts.dto.ResponseDto;
 import com.micro.accounts.entity.Accounts;
 import com.micro.accounts.service.IAccountsService;
 import com.micro.accounts.service.impl.AccountsServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -24,6 +28,10 @@ import java.lang.annotation.Repeatable;
 @RequestMapping(path = "/api",produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 @Validated
+@Tag(
+        name="CRUD REST API's for Accounts microservice",
+        description = "CRUD REST API's for creating/fetching/updating/deleting the customer and accounts data"
+)
 //In production ready applications it is always advised to have a prefix path to your controller class like we have /api and also
 //mention the return type of this controller means what type of return it give : produces means what it gives back
 //MediaType from org.springframework.http.MediaType then APPLICATION_JSON_VALUE means it will always return a json type value
@@ -33,6 +41,17 @@ public class AccountsController {
     @Qualifier("accountsServiceImplV1")
     private IAccountsService accountsServiceImpl;
 
+
+
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create and register new customer and account details "
+    )
+    @ApiResponse
+            (
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            )
     @PostMapping("/create")
     //we need PostMapping as this is a create operation
      //RequestBody has to be linked to the CustomerDto type POJO as it is the class responsible for carrying the customer details
@@ -55,6 +74,17 @@ public class AccountsController {
      *                     but if it was PathVariable then the URL would have looked like /fetch/{mobileNumber}
      * @return
      */
+
+    @Operation(
+            summary = "Fetch Customer and Account details from the DB",
+            description = "This REST API is responsible to return the Customer and Account details from the DB based on a Mobile Number sent as" +
+                    "RequestParam/QueryParam"
+    )
+    @ApiResponse
+            (
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
                                                                @NotEmpty(message = "Mobile number cannot be empty")
@@ -66,6 +96,28 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
+
+
+    @Operation(
+            summary = "Update Account details REST API",
+            description = "This REST API is responsible to update the Account details of a customer based on the provided Account Number " +
+                    " you have to pass both the customer as well as account details in a single JSON format"
+    )
+    @ApiResponses
+            (
+                    {
+                            @ApiResponse
+                                    (
+                                            responseCode = "200",
+                                            description = "HTTP Status OK"
+                                    ),
+                            @ApiResponse
+                                    (
+                                            responseCode = "500",
+                                            description = "HTTP Status Internal Server Error"
+                                    )
+                    }
+            )
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid@RequestBody CustomerDto customerDto)
     {
@@ -82,6 +134,30 @@ public class AccountsController {
         }
     }
 
+
+
+
+
+    @Operation(
+            summary = "Delete Account details REST API",
+            description = "This REST API is responsible to Delete the Customer and Account details of a customer based on the provided Account Number " +
+                    " you have to pass both the customer as well as account details in a single JSON format"
+    )
+    @ApiResponses
+            (
+                    {
+                            @ApiResponse
+                                    (
+                                            responseCode = "200",
+                                            description = "HTTP Status OK"
+                                    ),
+                            @ApiResponse
+                                    (
+                                            responseCode = "500",
+                                            description = "HTTP Status Internal Server Error"
+                                    )
+                    }
+            )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
                                                                 @NotEmpty(message = "Mobile number cannot be empty")
