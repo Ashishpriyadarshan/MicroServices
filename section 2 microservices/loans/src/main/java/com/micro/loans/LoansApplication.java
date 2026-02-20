75cf6a91fb2151af5c53cc493a2ee4f5b59a65d1
Ashish <ashishpriyadarshan2605@gmail.com>
2026-02-20 23:59:11 +0530

    Added OpenAPI Swagger specification dependency

--- section 2 microservices/loans/src/main/java/com/micro/loans/LoansApplication.java
+++ section 2 microservices/loans/src/main/java/com/micro/loans/LoansApplication.java
@@ -1,12 +1,18 @@
 package com.micro.loans;
 
 import com.micro.loans.audit.AuditAwareImpl;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+
 public class LoansApplication {
 
 	public static void main(String[] args) {
