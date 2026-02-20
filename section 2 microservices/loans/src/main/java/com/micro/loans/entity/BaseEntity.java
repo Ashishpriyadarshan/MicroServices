5b4b0aca84abfbe8562629c9e0435788f950401f
Ashish <ashishpriyadarshan2605@gmail.com>
2026-02-20 14:11:09 +0530

    Audit implemented | AuditAwareImpl introduced | JpaAuditingEnabled

--- section 2 microservices/loans/src/main/java/com/micro/loans/entity/BaseEntity.java
+++ section 2 microservices/loans/src/main/java/com/micro/loans/entity/BaseEntity.java
@@ -1,27 +1,38 @@
 package com.micro.loans.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 
 @Getter@Setter@ToString
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Column(name = "created_at")
+    @CreatedDate
     private LocalDateTime createdAt;
 
     @Column(name = "created_by")
+    @CreatedBy
     private String createdBy;
 
 
     @Column(name = "updated_at")
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     @Column(name = "updated_by")
+    @LastModifiedBy
     private String updatedBy;
 
 }
