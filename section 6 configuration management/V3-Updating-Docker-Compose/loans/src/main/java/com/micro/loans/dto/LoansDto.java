850d2b09470e2605dbfce0787a30bb5efe8c1a30
Ashish <ashishpriyadarshan2605@gmail.com>
2026-05-05 04:34:05 +0530

    Copied microservices from V2 folder | We will learn here how to modify the Docker Compose File

---
+++ section 6 configuration management/V3-Updating-Docker-Compose/loans/src/main/java/com/micro/loans/dto/LoansDto.java
@@ -0,0 +1,45 @@
+package com.micro.loans.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data@AllArgsConstructor@NoArgsConstructor
+@Schema(
+        name = "Loans Schema",
+        description = "This schema holds Loans Details"
+)
+public class LoansDto {
+
+
+    @NotEmpty(message = "Mobile Number is required")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile Number must be 10 digits")
+    private String mobileNumber;
+
+
+    @NotEmpty(message = "Loan Number cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{12})",message = "Loan Number must be 12 digits")
+    private String loanNumber;
+
+
+    @NotEmpty(message = "Loan Type cannot be Null or Empty")
+    private String loanType;
+
+
+    @PositiveOrZero(message = "Total Loan amount should be greater than 0")
+    private int totalLoan;
+
+
+    @PositiveOrZero(message = "Amount paid should be equal or greater than 0")
+    private int amountPaid;
+
+
+    @PositiveOrZero(message = "Total Outstanding amount should be equal or greater than zero")
+    private int outstandingAmount;
+
+}
